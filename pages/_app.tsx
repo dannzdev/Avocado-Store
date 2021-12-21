5f91b2db4be73880b0e3511a1ec0d35061ba716c
Daniel Sanchez <danielsanchez1502@gmail.com>
2021-12-21 16:18:07 -0300

    ssg, link , proxy y next-image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 import '../global.css'
-
 import CartProvider from '@store/Cart'
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
